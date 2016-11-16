859c63796815c22ca460f1b3ac959b97148609be
Adam Johnson <adamj537@gmail.com>
2016-11-16 15:39:53 -0600

    Edited macros to include parentheses around all arguments (this decreases the chance that the macro will do unexpected things if an expression instead of a constant is used as an argument). * Edited macros ISBITSET and ISMASKSET to return TRUE or FALSE instead of a bit mask.

--- Utilities/BitLogic.h
+++ Utilities/BitLogic.h
@@ -1,27 +1,6 @@
 #ifndef BITLOGIC_H
 #define BITLOGIC_H
 
-// Macros to do boolean calculations with a bit number
-#define SETBIT(BYTE, BIT)		(BYTE |= (1 << BIT))
-#define CLEARBIT(BYTE, BIT)		(BYTE &= ~(1 << BIT))
-#define TOGGLEBIT(BYTE, BIT)	(BYTE ^= (1 << BIT))
-#define ISBITSET(BYTE, BIT)		(BYTE & (1 << BIT))
-#define ISBITCLEAR(BYTE, BIT)	(!ISBITSET(BYTE, BIT))
-
-// Macro to convert a bit number into a byte value
-#define BV(bit)					(1 << (bit))
-
-// Macros to do boolean calculations with a byte value
-#define SETMASK(BYTE, MASK)		(BYTE |= (MASK))
-#define CLEARMASK(BYTE, MASK)	(BYTE &= ~(MASK))
-#define TOGGLEMASK(BYTE, MASK)	(BYTE ^= (MASK))
-#define ISMASKSET(BYTE, MASK)	(BYTE & (MASK))
-#define ISMASKCLEAR(BYTE, MASK)	(!ISMASKSET(BYTE, MASK))
-
-// Macros to figure out if an expression is even or odd
-#define ISODD(x)				((x) % 2)
-#define ISEVEN(x)				(!ISODD(x))
-
 // Definition for "TRUE" (because I hate that it's usually defined lowercase)
 #ifndef TRUE
 #define TRUE	1
@@ -31,5 +10,26 @@
 #ifndef FALSE
 #define FALSE	0
 #endif
+
+// Macros to do boolean calculations with a bit number
+#define SETBIT(BYTE, BIT)		((BYTE) |= (1 << (BIT)))
+#define CLEARBIT(BYTE, BIT)		((BYTE) &= ~(1 << (BIT)))
+#define TOGGLEBIT(BYTE, BIT)	((BYTE) ^= (1 << (BIT)))
+#define ISBITSET(BYTE, BIT)		((0 != ((BYTE) & (1 << (BIT)))) ? TRUE : FALSE)
+#define ISBITCLEAR(BYTE, BIT)	(!ISBITSET((BYTE), (BIT)))
+
+// Macro to convert a bit number into a byte value
+#define BV(bit)					(1 << (bit))
+
+// Macros to do boolean calculations with a byte value
+#define SETMASK(BYTE, MASK)		((BYTE) |= (MASK))
+#define CLEARMASK(BYTE, MASK)	((BYTE) &= ~(MASK))
+#define TOGGLEMASK(BYTE, MASK)	((BYTE) ^= (MASK))
+#define ISMASKSET(BYTE, MASK)	((0 != ((BYTE) & (MASK))) ? TRUE : FALSE)
+#define ISMASKCLEAR(BYTE, MASK)	(!ISMASKSET((BYTE), (MASK)))
+
+// Macros to figure out if an expression is even or odd
+#define ISODD(x)				((x) % 2)
+#define ISEVEN(x)				(!ISODD(x))
 
 #endif
