cc11c8354f1f8db54f61e807a92e32a4fd00cca5
Adam Johnson <ajohnson@dwyermail.com>
2017-09-07 09:42:31 -0500

    Added NOP() macro.

---
+++ Utilities/Nop.h
@@ -0,0 +1,19 @@
+#ifndef NOP_H
+#define NOP_H
+
+// Define no operation for TI & IAR compilers.
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#define NOP()	__no_operation()
+#endif
+
+// Define no operation for AVR compilers.
+#if defined (__AVR__)
+#define NOP()	do { __asm__ __volatile__ ("nop"); } while (0)
+#endif
+
+// Define no operation for PIC18 compilers.
+#if defined(__18CXX)
+#define NOP()	__nop()
+#endif
+
+#endif
