bd254a1040d34e88063428617babbc2e7211aa5c
Adam Johnson <adamj537@gmail.com>
2025-12-14 17:00:35 -0600

    Added project h file.

---
+++ Project/CC3200/project.h
@@ -0,0 +1,22 @@
+/******************************************************************************
+ *
+ *	Filename:		Project.h
+ *
+ *	Author:			Adam Johnson
+ *
+ *	Description:	Contains information and settings applicable to the entire
+ *					project.  This file should be included before any others
+ *					so that the symbols defined here are guaranteed to be
+ *					global.
+ *
+ *****************************************************************************/
+ 
+#ifndef PROJECT__H
+#define PROJECT__H
+
+#define INCLUDE_TEST			// Include test code.
+#define ID			"TEST"		// firmware ID
+#define FVN			"V1"		// firmware version
+#define XTAL_FREQ	80000000	// oscillator frequency [Hz]
+
+#endif
